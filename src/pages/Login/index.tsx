b41ca2b697c5ff9c481ac0b9a7e129807e79a192
Hayden Lane <hayden.inbox@gmail.com>
2024-10-07 22:02:26 -0500

    Add tailwind styling to login box

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,11 +5,17 @@ import { fetchWrapper, HttpMethod } from "@/lib/fetchData";
 function Login() {
     const [email, setEmail] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+    const [background, setBackground] = useState<string>("#FED7AA");
     const [error, setError] = useState<string | null>(null);
     const submitButton = document.getElementById("submit-button");
 
     async function handleSubmit(e) {
         e.preventDefault();
+
+        if (email.trim().length === 0) {
+            setMessage("Enter an email address.")
+            return
+        }
 
         const controller = new AbortController();
 
@@ -26,32 +32,52 @@ function Login() {
                 });
 
             if (response.status == 400) {
+                setBackground("#FED7AA")
                 setMessage("Enter an email address.")
             }
             else if (response.status == 404) {
+                setBackground("#FED7AA")
                 setMessage("Account not found with that email.")
             }
             else if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             } else {
+                setBackground("#A7F3D0")
                 const json = await response.json();
                 setMessage(json.message);
             }
-        } catch (e) {
-            console.log("An error occurred:", e);
+        } catch (error) {
+            setError((error as Error).message)
         }
     }
 
     return <>
         <TitleBar title={"Login"} />
-        <div className="content">
-            <div className="flex flex-col items-center justify-center h-full">
-                {message ? <span className="p-3 bg-orange-200">{message}</span> : ""}
-                <p><strong>To get started, enter your email address.</strong><br />
-                    We'll send you a link you can use to login. </p>
+        <div className="content flex items-center justify-center h-full">
+            <div className="flex flex-col space-y-4 bg-white p-6 rounded text-center w-full max-w-md">
+                {message ? <span style={{ backgroundColor: `${background}` }} className={`p-1 w-full`}>{message}</span> : ""}
+                <p className="text-lg font-semibold">To get started, enter your email address.</p>
+                <p>We'll send you a link you can use to login.</p>
                 <form action="submit">
-                    <input type="email" name="email" id="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required /><br />
-                    <button type="submit" id="submit-button" onClick={handleSubmit}>Sign In</button>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="border border-gray-300 rounded px-3 py-2 w-full"
+                    />
+                    <div className="flex justify-end py-2">
+                        <button
+                            className="button-primary"
+                            type="submit"
+                            id="submit-button"
+                            onClick={handleSubmit}
+                        >
+                            Sign In
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>

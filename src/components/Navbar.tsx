7a4fdbc12fe5a666f7f9b44c3e67a57dbec727bf
Hayden Lane <hayden.inbox@gmail.com>
2024-11-18 23:12:48 -0600

    Fix Navbar showing links.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,64 +23,69 @@ function Navbar() {
                     <img src={logo} alt="Logo" id="logo" />
                     <span className="text-2xl">benevolence</span>
                 </Link>
+                {
+                    !isLoggedIn ? null : (
+                        <>
+                            {/* Menu button (for mobile) */}
+                            <div className="sm:hidden">
+                                <button
+                                    onClick={toggleMenu}
+                                    type="button"
+                                    className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white"
+                                    aria-controls="mobile-menu"
+                                    aria-expanded={isOpen}
+                                >
+                                    <svg
+                                        className="block h-6 w-6"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        {isOpen ? (
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        ) : (
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M4 6h16M4 12h16M4 18h16"
+                                            />
+                                        )}
+                                    </svg>
+                                </button>
+                            </div>
 
-                {/* Menu button (for mobile) */}
-                <div className="sm:hidden">
-                    <button
-                        onClick={toggleMenu}
-                        type="button"
-                        className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white"
-                        aria-controls="mobile-menu"
-                        aria-expanded={isOpen}
-                    >
-                        <svg
-                            className="block h-6 w-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            {isOpen ? (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            ) : (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            )}
-                        </svg>
-                    </button>
-                </div>
-
-                {/* Navbar links (visible on larger screens) */}
-                <div className="hidden sm:flex space-x-4">
-                    <Link to="/" className="hover:text-[#a2cd5a]">Home</Link>
-                    {isAdmin ? <Link to="/admin" className="hover:text-[#a2cd5a]">Admin</Link> : null}
-                    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-                        <DialogTrigger>
-                            <a href="#" className="hover:text-[#a2cd5a]">Logout</a>
-                        </DialogTrigger>
-                        <DialogContent>
-                            <LogoutDialog onOpenChange={setDialogOpen} />
-                        </DialogContent>
-                    </Dialog>
-                </div>
+                            {/* Navbar links (visible on larger screens) */}
+                            <div className="hidden sm:flex space-x-4">
+                                <Link to="/" className="hover:text-[#84A296]">Home</Link>
+                                {isAdmin ? <Link to="/admin" className="hover:text-[#84A296]">Admin</Link> : null}
+                                <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                                    <DialogTrigger>
+                                        <a href="#" className="hover:text-[#84A296]">Logout</a>
+                                    </DialogTrigger>
+                                    <DialogContent>
+                                        <LogoutDialog onOpenChange={setDialogOpen} />
+                                    </DialogContent>
+                                </Dialog>
+                            </div>
+                        </>
+                    )
+                }
             </div>
 
             {/* Mobile menu */}
-            {isOpen && (
+            {(isOpen && isLoggedIn) && (
                 <div className="sm:hidden" id="mobile-menu">
                     <div className="px-2 pt-2 pb-3 space-y-1">
                         <Link
                             to="/"
-                            className="block px-3 py-2 rounded-md text-base font-medium hover:text-[#a2cd5a]"
+                            className="block px-3 py-2 rounded-md text-base font-medium hover:text-[#84A296]"
                             onClick={() => toggleMenu()}
                         >
                             Home
@@ -88,13 +93,13 @@ function Navbar() {
                         {isAdmin ?
                             <Link
                                 to="/admin"
-                                className="block px-3 py-2 rounded-md text-base font-medium hover:text-[#a2cd5a]"
+                                className="block px-3 py-2 rounded-md text-base font-medium hover:text-[#84A296]"
                                 onClick={() => toggleMenu()}
                             >
                                 Admin
                             </Link> : null}
                         <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-                            <DialogTrigger className="w-full hover:text-[#a2cd5a]">
+                            <DialogTrigger className="w-full hover:text-[#84A296]">
                                 <button
                                     className="block px-3 py-2 rounded-md text-base font-medium"
                                     onClick={() => toggleMenu()}
